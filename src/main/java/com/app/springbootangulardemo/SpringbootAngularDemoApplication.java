0ff560d7cf45b3f71d2d542c102cb0b711424378
jc <jc@10.0.0.13>
2020-06-15 23:37:57 -0400

    added asynchronous email processing

--- src/main/java/com/app/springbootangulardemo/SpringbootAngularDemoApplication.java
+++ src/main/java/com/app/springbootangulardemo/SpringbootAngularDemoApplication.java
@@ -2,8 +2,10 @@ package com.app.springbootangulardemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringbootAngularDemoApplication {
 
 	public static void main(String[] args) {
